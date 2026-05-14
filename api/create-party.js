d843fae97f56858ab9fe2a3a597e08e91b589fa6
aneta0412 <mailforaneta@yahoo.com>
2026-05-14 10:24:45 +0100

    Update create-party.js

--- api/create-party.js
+++ api/create-party.js
@@ -10,7 +10,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail({ to, subject, html }) {
   return resend.emails.send({
-    from: 'Tiny Invites <onboarding@resend.dev>',
+    from: 'Tiny Invites <hello@tinyinvites.org>',
     to,
     subject,
     html,

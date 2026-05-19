d92c38fd37ceeca4a34d273ee26adf6f8ca618ba
aneta0412 <mailforaneta@yahoo.com>
2026-05-19 13:41:07 +0100

    Delete api/debug.js

--- api/debug.js
+++
@@ -1,9 +0,0 @@
-console.log("debug.js loaded");
-
-window.runAllTests = function () {
-  alert("Run All Tests button works");
-};
-
-window.clearResults = function () {
-  alert("Clear button works");
-};

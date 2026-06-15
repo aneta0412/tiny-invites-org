f9f9e072ca97106d7537e86c6dc76b7a27590086
aneta0412 <mailforaneta@yahoo.com>
2026-06-15 14:32:40 +0100

    Add files via upload

--- api/dashboard-api.js
+++ api/dashboard-api.js
@@ -1,10 +1,12 @@
 // Updated: 2026-06-11
-// Router: /api/dashboard-api?action=load | update-guest | send-email | save-reminder-note | send-update
+// Router: /api/dashboard-api?action=load | update-guest | send-email | save-reminder-note | send-update | extend-cutoff | update-party
 import loadDashboard    from '../lib/dashboard.js';
 import updateGuest      from '../lib/update-guest.js';
 import sendGuestList    from '../lib/send-email.js';
 import saveReminderNote from '../lib/save-reminder-note.js';
 import sendUpdate       from '../lib/send-update.js';
+import extendCutoff     from '../lib/extend-cutoff.js';
+import updateParty      from '../lib/update-party.js';
 
 const routes = {
   'load':               loadDashboard,
@@ -12,6 +14,8 @@ const routes = {
   'send-email':         sendGuestList,
   'save-reminder-note': saveReminderNote,
   'send-update':        sendUpdate,
+  'extend-cutoff':      extendCutoff,
+  'update-party':       updateParty,
 };
 
 export default async function handler(req, res) {
